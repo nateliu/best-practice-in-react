0ba33361a4988923cf980ff111dafd0364dfcdad
Nate Liu <jinlliu@expediagroup.com>
2022-01-23 17:03:22 +0800

    remove nodejs-projects from this monorepos

--- packages/nodejs-projects/First/Index.js
+++
@@ -1,8 +0,0 @@
-﻿var server = require("./Server");
-var route = require("./Router");
-var handler = require("./Handlers");
-var handle = {};
-handle["/"] = handler.start;
-handle["/start"] = handler.start;
-handle["/upload"] = handler.upload;
-server.start(route.route, handle);
